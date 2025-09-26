7b9b2a1c46ce684b706f5f327b565af23a1652e9
gavhaneyash27 <gavhaneyash27@gmail.com>
2025-09-26 12:04:18 +0530

    Add files via upload

---
+++ Databricks-notebooks/dim_event.py
@@ -0,0 +1,165 @@
+# Databricks notebook source
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Create incremental flag
+
+# COMMAND ----------
+
+dbutils.widgets.text('incremental_flag','0')
+
+# COMMAND ----------
+
+incremental_flag = dbutils.widgets.get('incremental_flag')  
+print(incremental_flag)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Query data for reference
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from parquet.`abfss://silver@olympicsdatalake7.dfs.core.windows.net/transformed_data`
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Create source df
+
+# COMMAND ----------
+
+df_src = spark.sql('''
+                   select distinct(event) as event, sport
+                   from parquet.`abfss://silver@olympicsdatalake7.dfs.core.windows.net/transformed_data`
+                   ''')
+
+
+
+# COMMAND ----------
+
+df_src.display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Create sink df - For initial and incremental run
+
+# COMMAND ----------
+
+if spark.catalog.tableExists('olympic_catalog.gold.dim_event'):
+    df_sink = spark.sql('''
+                        select dim_event_key, event, sport
+                        from olympic_catalog.gold.dim_event
+                        ''')
+else:
+    df_sink = spark.sql('''
+                        select 1 as dim_event_key, event, sport
+                        from parquet.`abfss://silver@olympicsdatalake7.dfs.core.windows.net/transformed_data`
+                        where 1 = 0
+                        ''')
+
+# COMMAND ----------
+
+df_sink.display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Filter old and new data
+
+# COMMAND ----------
+
+df_filter = df_src.join(df_sink, df_src['event'] == df_sink['event'],'left').select(df_src['event'],df_src['sport'],\
+                   df_sink['dim_event_key'])
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### df_filter_old
+
+# COMMAND ----------
+
+df_filter_old = df_filter.filter(df_filter['dim_event_key'].isNotNull())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### df_filter_new
+
+# COMMAND ----------
+
+df_filter_new = df_filter.filter(df_filter['dim_event_key'].isNull())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Fetch max surrogate key
+
+# COMMAND ----------
+
+if incremental_flag == '0':
+    max_value = 0
+
+else:
+    max_value_df = spark.sql('''
+                             select max(dim_event_key) from olympic_catalog.gold.dim_event
+                             ''')
+    max_value = max_value_df.collect()[0][0]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Add the max surrogate key in surrogate key column
+
+# COMMAND ----------
+
+df_filter_new = df_filter_new.withColumn('dim_event_key',max_value + monotonically_increasing_id()+1)
+
+# COMMAND ----------
+
+df_filter_new.display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Final_df
+
+# COMMAND ----------
+
+df_final = df_filter_old.union(df_filter_new)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # SCD TYPE 1
+
+# COMMAND ----------
+
+from delta.tables import DeltaTable
+
+# COMMAND ----------
+
+if spark.catalog.tableExists('olympic_catalog.gold.dim_event'):
+    delta_tbl = DeltaTable.forPath(spark,'abfss://gold@olympicsdatalake7.dfs.core.windows.net/dim_event')
+
+    delta_tbl.alias('trg').merge(df_final.alias('src'), 'trg.event = src.event')\
+                          .whenMatchedUpdateAll()\
+                          .whenNotMatchedInsertAll()\
+                          .execute()
+
+else:
+    df_final.write.format('delta')\
+                  .mode('overwrite')\
+                  .option('path','abfss://gold@olympicsdatalake7.dfs.core.windows.net/dim_event')\
+                  .saveAsTable('olympic_catalog.gold.dim_event')
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from olympic_catalog.gold.dim_event
